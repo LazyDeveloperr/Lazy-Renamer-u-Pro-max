22e4ebad5b540a31b5bb8d31010a0d1d0b7fde66
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-11-13 20:17:42 +0530

    Released Beta version 1.0.0

---
+++ bot.py
@@ -0,0 +1,38 @@
+from pyrogram import Client 
+from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            name="renamer",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
+    async def start(self):
+       await super().start()
+       me = await self.get_me()
+       self.mention = me.mention
+       self.username = me.username 
+       self.force_channel = FORCE_SUB
+       if FORCE_SUB:
+         try:
+            link = await self.export_chat_invite_link(FORCE_SUB)                  
+            self.invitelink = link
+         except Exception as e:
+            print(f"{e}")
+            print("Make Sure Bot is admin in force sub channel")             
+            self.force_channel = None
+       print(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
+       
+    async def stop(self, *args):
+      await super().stop()      
+      print("Bot Stopped")
+        
+bot = Bot()
+bot.run()
---
+++ helper/txt.py
@@ -0,0 +1,82 @@
+"""
+Apache License 2.0
+
+Copyright (c) 2022 @PYRO_BOTZ
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+Telegram Link : https://t.me/PYRO_BOTZ 
+Repo Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT
+License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICENSE
+"""
+
+
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰ Lazy Progress BAR ❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 : <a href=https://t.me/LazyDeveloper>LazyDeveloper</a> 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://github.com/LazyDeveloper>LazyDeveloper</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁1 : <a href=https://apps.koyeb.com>𝙷𝙴𝚁𝙾𝙺𝚄</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁2 : <a href=https://dashboard.heroku.com>KOYEB</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v3.6.8               
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO SET THUMBNAIL</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnail.
+•> /delthumb use this command and delete your old thumbnail.
+•> /viewthumb use this command to view your current thumbnail.
+
+📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>HOW TO RENAME A FILE</u></b>
+•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
+ℹ️ For more help:- <a href=https://t.me/LazyPrincessSupport>Support BOX</a>
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>Special Thanks & Developers</b></u> 
+
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/LazyDeveloperr/Lazy-Renamer-u-Pro-max>Lazy Renamer u-Pro-max</a>
+» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://youtu.be/GfulqsSnTv4>Watch Tutorial</a>
+
+• 😈 <a href=https://github.com/LazyDeveloperr>LazyDeveloper Github</a>
+• 😈 <a href=https://t.me/LazyDeveloper>LazyDeveloper Tg Channel</a>
+• 😈 <a href=https://t.me/mRiderDM>LazyDeveloper Direct DM</a>
+• 😈 <a href=https://t.me/real_MoviesAdda1>Movies Adda</a>
+• 😈 <a href=https://t.me/GitHub_noob>Thanks- Github Noob</a>
+"""
